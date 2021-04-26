26543713debd4a83d949a0d53c1c1ed6fb6602b1
Arshad <49522121+arshad-k7@users.noreply.github.com>
2021-04-26 17:31:37 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+//
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_6084557f5eb20e09cf363b7c';
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6084557f5eb20e09cf363b7c/1f44aan4g';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);})();
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  x.style.display = "none";
+  document.getElementById("div").style.display = "block";
+
+}
